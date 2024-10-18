03ce00a2d3c593a757b7e3c9b1cbdffd4a064b17
Clyde Mar <clydemar234@gmail.com>
2024-10-18 18:17:27 +0800

    chore: Add typedef declarations

---
+++ include/utils/types.h
@@ -0,0 +1,17 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef signed short i16;
+typedef signed int i32;
+typedef signed long i64;
+
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long u64;
+
+typedef float f32;
+typedef double f64;
+typedef long double f128;
+
+
+#endif
